3bcd8d7536019784a180344f9197110f49daf0f2
Gustavo Martini <38445129+martinigustavo@users.noreply.github.com>
2021-05-03 22:40:10 -0300

    :sparkles: Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,25 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">
+                    App desenvolvido por{" "}
+                    <a
+                        href="https://github.com/martinigustavo"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Gustavo Martini
+                    </a>
+                </p>
+                <p className="text-light">
+                    <small>
+                        <strong>Semana Spring React - DevSuperior</strong>
+                    </small>
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
